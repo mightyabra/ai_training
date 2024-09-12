300fda170745127ae1685665a095a7af480bc7fc
Mighty A. Ayidzoe <mighty.ayidzoe@uds.edu.gh>
2024-09-12 12:46:49 +0000

    Add files via upload

--- leafclassify.py
+++ leafclassify.py
@@ -22,7 +22,23 @@ st.header(':white[Maize Disease/Pest Detection App]')
 #st.markdown("Prediction Platform")
 
 
+def set_background(main_bg):  # local image
+    set bg name
+    main_bg_ext = "png"
+    st.markdown(
+        f"""
+             <style>
+             .stApp {{
+                 background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
+                 background-size: cover
+             }}
+             </style>
+             """,
+        unsafe_allow_html=True
+    )
 
+
+set_background('maize.png')
 
 
 def main():
